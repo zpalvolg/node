b49a78131036c850d8e5789a4db39b3cb41e5f32
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-02-19 18:38:28 +0100

    narrow down habitable planets in results array

--- planets/index.js
+++ planets/index.js
@@ -3,18 +3,25 @@ const fs = require('fs');
 
 const results = [];
 
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED' //Exoplanet Archive Disposition
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 //Insolation Flux 
+        && planet['koi_prad'] < 1.6; //Planetary Radius
+}
+
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({ //pipe createReadStream to parse as readable.pipe(writeable)
     comment: '#', //specify comments lines in csv file
     columns: true //each row will return as a js object
   }))
   .on('data',(data) => { //.on(event,callback function)
-    results.push(data);
+    if(isHabitablePlanet(data)){
+        results.push(data);
+    }
   })
   .on('error',(err) => {
     console.log(err);
   })
   .on('end',() => {
-    console.log(results);
-    console.log('done');
+    console.log(`${results.length} habitable planets found!`);
   });
