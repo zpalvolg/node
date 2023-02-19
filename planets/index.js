ddae8c9598e9238a2262b0eb0b41860cef7b2709
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-02-19 17:44:48 +0100

    add parse functionality

--- planets/index.js
+++ planets/index.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 const results = [];
 
 fs.createReadStream('kepler_data.csv')
-  .on('data',(data) => {
+  .pipe(parse({ //pipe createReadStream to parse as readable.pipe(writeable)
+    comment: '#', //specify comments lines in csv file
+    columns: true //each row will return as a js object
+  }))
+  .on('data',(data) => { //.on(event,callback function)
     results.push(data);
   })
   .on('error',(err) => {
