40a49ec72de96b88fcc4494c6b2d3dd94ce6fb08
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-02-20 18:47:36 +0100

    add routing based on request url

--- http_server/index.js
+++ http_server/index.js
@@ -2,18 +2,41 @@ const http = require('http');
 
 const PORT = 3000;
 
-const  server = http.createServer((req,res) =>{
-    res.writeHead(200, { //status code 200 = OK
-        'Content-Type': 'application/json',
-    })
+const  server = http.createServer();
 
-    res.end(JSON.stringify( //string is expected we should use stringify function
+server.on('request',(req,res) =>{ //request events
+
+    if (req.url === '/users'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        
+        res.write(JSON.stringify( //string is expected we should use stringify function
         {
             id: 1,
             name: 'Zoltan',
         }
-    ));
-});
+        ));
+
+        res.end();
+    }
+    else if(req.url === '/messages'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type','text/html');
+
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<h1>Hello World!</h1>');
+        res.write('</body>');
+        res.write('</html>');
+
+        res.end()
+    }
+    else{
+        res.statusCode = 404; //not found
+
+        res.end()
+    }
+})
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
