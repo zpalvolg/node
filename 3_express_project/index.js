2ebffd7ee18c3ecc789f474053e939d900319e97
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-02-22 19:22:58 +0100

    add route parameters

--- 3_express_project/index.js
+++ 3_express_project/index.js
@@ -4,8 +4,46 @@ const PORT = 3000;
 
 const server = express();
 
+const users = [
+    {
+        id: 0,
+        name: 'Attila',
+    },
+    {
+        id: 1,
+        name: 'Zoltan',
+    },
+    {
+        id: 2,
+        name: 'Laszlo',
+    }
+
+]
+
 server.get('/',(req,res) => {
     res.send('Hellow World!');
+});
+
+server.get('/users',(req,res) => {
+    res.send(users);
+});
+
+server.get('/users/:userId',(req,res) => {
+    
+    const userID = Number(req.params.userId);
+
+    const user = users[userID];
+
+    if(user){
+        res.status(200).json(user);
+    }else {
+        res.status(404).json(
+            {
+                error: "User does not exist!"
+            }
+        );
+    }
+    
 });
 
 server.listen(PORT, () => {
