c1fb2b2485c0370ed5d999e25fdf9ed940b60640
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-03-08 19:14:43 +0100

    added addNewLaunch validation

--- practice3_expressjs_and_react/server/src/routes/launches/launches.controller.js
+++ practice3_expressjs_and_react/server/src/routes/launches/launches.controller.js
@@ -7,8 +7,24 @@ function getAllLaunches(req, res) {
 function addNewLaunch(req, res){
     const launch = req.body;
 
+    // validation: Missing required property
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target){
+        //400 = bad request
+        return res.status(400).json({
+            error: 'Missing required property!'
+        });
+    }
+
     //convert strin from JSON to date
     launch.launchDate = new Date(launch.launchDate);
+
+    // validation: Invalid launch date
+    if(isNaN(launch.launchDate)){
+        //400 = bad request
+        return res.status(400).json({
+            error: 'Invalid launch date!'
+        });
+    }
 
     launchesModel.addNewLaunch(launch);
 
